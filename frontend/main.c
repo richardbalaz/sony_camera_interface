6e0a43be8079e148bf8757c2361273e51fdc7b8c
Richard Baláž <richard.balaz@proforce-products.com>
2017-06-17 12:14:00 +0200

    I heard that spaces are more cool than tabs

--- frontend/main.c
+++ frontend/main.c
@@ -9,50 +9,50 @@
 int encoder_previous_state = 0;
 
 enum commands {
-	ENCODER_INC = 0x00,
-	ENCODER_DEC = 0x01
+    ENCODER_INC = 0x00,
+    ENCODER_DEC = 0x01
 };
 
 /* Transmits 5 LSB bits to transmit line */
 void shift_data_out(uint8_t data)
 {
-	PORTB &= ~(1 << PB2);
-	_delay_us(CLK_DELAY);
-	int i;
-	for (i = 0x01; i <= 0x10; i <<= 1) {
-		if (data & i)
-			PORTB |= (1 << PB2);
-		else
-			PORTB &= ~(1 << PB2);
-		_delay_us(CLK_DELAY);
-	}
-	PORTB |= (1 << PB2);
+    PORTB &= ~(1 << PB2);
+    _delay_us(CLK_DELAY);
+    int i;
+    for (i = 0x01; i <= 0x10; i <<= 1) {
+        if (data & i)
+            PORTB |= (1 << PB2);
+        else
+            PORTB &= ~(1 << PB2);
+        _delay_us(CLK_DELAY);
+    }
+    PORTB |= (1 << PB2);
 }
 
 /* Checks if there is a rotation made by human */
 void read_rotary_encoder(void)
 {
-	int pin_a = PINB & (1 << PB0);
-	if (pin_a == 0 && encoder_previous_state != 0) {
-		if ((PINB & (1 << PB1)) == 0)
-			shift_data_out(ENCODER_DEC);
-		else
-			shift_data_out(ENCODER_INC);
-	}
-	encoder_previous_state = pin_a;
+    int pin_a = PINB & (1 << PB0);
+    if (pin_a == 0 && encoder_previous_state != 0) {
+        if ((PINB & (1 << PB1)) == 0)
+            shift_data_out(ENCODER_DEC);
+        else
+            shift_data_out(ENCODER_INC);
+    }
+    encoder_previous_state = pin_a;
 }
 
 int main(void)
 {
-	/* Setup input ports for rotary encoder */
-	PORTB |= (1 << PB0) | (1 << PB1);
-	DDRB &= ~((1 << PB0) | (1 << PB1));
+    /* Setup input ports for rotary encoder */
+    PORTB |= (1 << PB0) | (1 << PB1);
+    DDRB &= ~((1 << PB0) | (1 << PB1));
 
-	/* Setup output port for transmit line */
-	PORTB |= (1 << PB2);
-	DDRB |= (1 << PB2);
+    /* Setup output port for transmit line */
+    PORTB |= (1 << PB2);
+    DDRB |= (1 << PB2);
 
-	while (1) {
-		read_rotary_encoder();
-	}
+    while (1) {
+        read_rotary_encoder();
+    }
 }
